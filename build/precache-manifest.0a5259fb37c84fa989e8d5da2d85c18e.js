9710bd9e63bf820229d2b36675e1c3c75a5682bb
flexiblefactory <hal@Hals-MacBook-Pro.local>
2020-05-08 20:32:40 +0100

    Build

---
+++ build/precache-manifest.0a5259fb37c84fa989e8d5da2d85c18e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628e6b9e7420e3ea4f1ee4af1f39457a",
+    "url": "./build/index.html"
+  },
+  {
+    "revision": "ea1a80766405505a7b06",
+    "url": "./build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fa88d02d20034807eeee",
+    "url": "./build/static/js/2.4db05e58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./build/static/js/2.4db05e58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1a80766405505a7b06",
+    "url": "./build/static/js/main.10bbfdec.chunk.js"
+  },
+  {
+    "revision": "8f733a8a83fba991275d",
+    "url": "./build/static/js/runtime-main.71b7fb6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./build/static/media/logo.5d5d9eef.svg"
+  }
+]);
